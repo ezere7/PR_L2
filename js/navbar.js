15d3c7b6714c2b5f502c587023808ec5351e2cc5
Mariusz Kurek <mariusz53710@gmail.com>
2021-03-19 14:35:01 +0100

    komponent js

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 let nav = '<nav><a href="index.html">Home </a>'+
-    '<a href="kontakt.html">Kontakt</a> </nav>';
+    '<a href="contakt.html">Kontakt</a> </nav>';
 let navbarElem = document.querySelector(".navbar"); //lub inny sposób opisany wyżej
 
 navbarElem.innerHTML = nav;
